5a28317ec20b106db51fe478bd262f280ff37e02
blackboxsql <pythonboy007@gmail.com>
2021-08-30 22:34:46 +0600

    terminal git command

--- potosio/potosioBackend/views.py
+++ potosio/potosioBackend/views.py
@@ -2,7 +2,3 @@ from django.shortcuts import render
 
 # Create your views here.
 
-class ModelNameViewSet(viewsets.ModelViewSet):
-
-    queryset = ModelName.objects.all()
-    serializer_class = ModelNameSerializer
